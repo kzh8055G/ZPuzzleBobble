9b2559f41ebf246bf0b012dc4a92de8b809db6e0
kzh8055G <kzh8055@naver.com>
2021-03-20 21:20:37 +0900

    편집한 레벨 데이터가 모바일에서 제대로 재생되지 않던 버그 수정

--- Assets/Scripts/ManageLevelData.cs
+++ Assets/Scripts/ManageLevelData.cs
@@ -1,61 +1,62 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
+// data save with binaryformatter
 // https://gamedevelopment.tutsplus.com/tutorials/how-to-save-and-load-your-players-progress-in-unity--cms-20934
 
+// * 모바일에선 Application.dataPath, Application.persistentDataPath 디렉토리 외엔 데이터를
+// 저장할수 없다
 public static class ManageLevelData
 {
-	private static string levelDataFilePath => Path.Combine(Application.dataPath, @"levels.json");
+	private static string levelDataFilePath => Path.Combine(Application.persistentDataPath, @"levels.json");
 	public static Dictionary<int, Level> Levels = new Dictionary<int, Level>();
 
+	#region Default Level Data Path
+
+	private static readonly string pathDefaultLevelDataSave = @"Assets/Resources/Data/default_levels.json";
+	private static readonly string pathDefaultLevelDataLoad = @"Data/default_levels";
+	#endregion
+	
 	public static bool GetLevel(int _levelNumber, out Level _level)
 	{
 		return Levels.TryGetValue(_levelNumber, out _level);
 	}
-
 	public static void SaveToFile()
 	{
+		if (!File.Exists(levelDataFilePath))
+		{
+			Debug.Log("No Level Data so Create");
+		}
 		var jsonInfo = JObject.FromObject(Levels);
 		File.WriteAllText(levelDataFilePath, jsonInfo.ToString());
-
-        //BinaryFormatter bf = new BinaryFormatter();
-        //FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
-        //bf.Serialize(file, ManageLevelData.Levels);
-        //file.Close();
-    }
-
+#if UNITY_EDITOR
+		File.WriteAllText(pathDefaultLevelDataSave, jsonInfo.ToString());
+#endif
+	}
+	
 	public static bool LoadFromFile()
 	{
-		var pathLevelData = Path.Combine(Application.dataPath, "levels.json");
-		if( File.Exists(pathLevelData))
-        {
-			var jsonString = File.ReadAllText(pathLevelData);
+		if (File.Exists(levelDataFilePath))
+		{
+			var jsonString = File.ReadAllText(levelDataFilePath);
 			if (!string.IsNullOrEmpty(jsonString))
 			{
 				ManageLevelData.Levels = JsonConvert.DeserializeObject<Dictionary<int, Level>>(jsonString);
 				return true;
 			}
 		}
+		// 데이터가 존재하지 않는다면 (설치 이후 최초인 상태 실행했을시)
+		// 일단 resources 폴더에 존재하는 기본 데이터를 이용한다
+		TextAsset level = Resources.Load<TextAsset>(pathDefaultLevelDataLoad);
+		if (level != null)
+		{
+			ManageLevelData.Levels = JsonConvert.DeserializeObject<Dictionary<int, Level>>(level.text);
+			SaveToFile();
+			return true;
+		}
 		return false;
-
-		//catch(Exception _e)
-  //      {
-  //          if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
-  //          {
-  //              BinaryFormatter bf = new BinaryFormatter();
-  //              FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
-  //              file.Seek(0, SeekOrigin.Begin);
-  //              ManageLevelData.Levels = (Dictionary<int, Level>)bf.Deserialize(file);
-  //              file.Close();
-  //              return true;
-  //          }
-  //      }
-  //		return false;
 	}
 }
